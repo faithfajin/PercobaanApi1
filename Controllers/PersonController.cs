ee9e78b685a224877dc8a353813e855478346290
Faith Reyhan -2082 <faithfrew21@gmail.com>
2024-03-08 17:28:18 +0700

    Add project files.

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PercobaanApi1.Models;
+
+namespace PercobaanApi1.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("api/person")]
+        public ActionResult<Person> ListPerson()
+        {
+            PersonContext context = new PersonContext();
+            List<Person> ListPerson = context.ListPerson();
+            return Ok(ListPerson);
+        }
+
+
+        [HttpGet("api/person/{id}")]
+        public ActionResult GetPersonId(int id)
+        {
+            PersonContext context = new PersonContext();
+            Person filterid = context.GetPersonId(id);
+            
+            if (filterid != null)
+            {
+                return Ok(filterid);
+            }
+            else
+            {
+                return NotFound(); 
+            }
+        }
+    }
+}
